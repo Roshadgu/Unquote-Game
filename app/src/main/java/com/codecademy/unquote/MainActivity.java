39d09a4a3262cfd87c6c5cd7a70f725be71dcd97
Roshad Guerrier <Roshadgu@gmail.com>
2020-07-06 12:30:16 -0400

    Playable... Still need to incorporate rounds.

--- app/src/main/java/com/codecademy/unquote/MainActivity.java
+++ app/src/main/java/com/codecademy/unquote/MainActivity.java
@@ -97,11 +97,11 @@ public class MainActivity extends AppCompatActivity
         // TODO 5-A: set onClickListener for the submit answer Button
         submitButton.setOnClickListener(new View.OnClickListener()
         {
-            @Override
-            public void onClick(View view)
-            {
-                onAnswerSubmission();
-            }
+          @Override
+          public void onClick(View view)
+          {
+            onAnswerSubmission();
+          }
         });
 
         startNewGame();
@@ -174,40 +174,54 @@ public class MainActivity extends AppCompatActivity
     void onAnswerSubmission()
     {
         Question currentQuestion = getCurrentQuestion();
-        if (currentQuestion.isCorrect())
+
+        if(currentQuestion.playerAnswer == -1)
         {
-            totalCorrect = totalCorrect + 1;
+            AlertDialog.Builder submitError = new AlertDialog.Builder(MainActivity.this);
+            submitError.setCancelable(true);
+            submitError.setTitle("No Answer Selected");
+            submitError.setMessage("Before submitting please select an answer to submit");
+
+            submitError.create().show();
         }
-        questions.remove(currentQuestion);
 
-        // TODO 3-D.i: Uncomment the line below after implementing displayQuestionsRemaining(int)
-        displayQuestionsRemaining(questions.size());
-
-        if (questions.size() == 0)
-        {
-            String gameOverMessage = getGameOverMessage(totalCorrect, totalQuestions);
-
-            // TODO 5-D: Show a popup instead
-            AlertDialog.Builder gameOverDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-            gameOverDialogBuilder.setCancelable(false);
-            gameOverDialogBuilder.setTitle("Gamer Over!");
-            gameOverDialogBuilder.setMessage("The game is over. Do you want to try again?");
-            gameOverDialogBuilder.setPositiveButton("Play Again!", new DialogInterface.OnClickListener()
-            {
-                @Override
-                public void onClick(DialogInterface dialog, int which)
-                {
-                    startNewGame();
-                }
-            });
-            gameOverDialogBuilder.create().show();
-        }
         else
         {
-            chooseNewQuestion();
+            if (currentQuestion.isCorrect())
+            {
+                totalCorrect = totalCorrect + 1;
+            }
+            questions.remove(currentQuestion);
 
-            // TODO 3-H.i: uncomment after implementing displayQuestion(Question)
-            displayQuestion(getCurrentQuestion());
+            // TODO 3-D.i: Uncomment the line below after implementing displayQuestionsRemaining(int)
+            displayQuestionsRemaining(questions.size());
+
+            if (questions.size() == 0)
+            {
+                String gameOverMessage = getGameOverMessage(totalCorrect, totalQuestions);
+
+                // TODO 5-D: Show a popup instead
+                AlertDialog.Builder gameOverDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                gameOverDialogBuilder.setCancelable(false);
+                gameOverDialogBuilder.setTitle("Gamer Over!");
+                gameOverDialogBuilder.setMessage("The game is over. Do you want to try again?");
+                gameOverDialogBuilder.setPositiveButton("Play Again!", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        startNewGame();
+                    }
+                });
+                gameOverDialogBuilder.create().show();
+            }
+            else
+            {
+                chooseNewQuestion();
+
+                // TODO 3-H.i: uncomment after implementing displayQuestion(Question)
+                displayQuestion(getCurrentQuestion());
+            }
         }
     }
 
@@ -216,12 +230,20 @@ public class MainActivity extends AppCompatActivity
         questions = new ArrayList<>();
 
         // TODO 2-H: Provide actual drawables for each of these questions!
-        Question question0 = new Question(R.drawable.img_quote_0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question1 = new Question(R.drawable.img_quote_1, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question2 = new Question(R.drawable.img_quote_2, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question3 = new Question(R.drawable.img_quote_3, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question4 = new Question(R.drawable.img_quote_4, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question5 = new Question(R.drawable.img_quote_5, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question0 = new Question(R.drawable.img_quote_0, "Pretty good advice, and perhaps a scientist did say it... Who actually did?", "Albert Einstein", "Isaac Newton ", "Rita Mae Brown", "Rosalind Franklin", 3);
+        Question question1 = new Question(R.drawable.img_quote_1, "Was honest Abe honestly quoted? Who authored this pithy bit of wisdom?", "Edward Stieglitz", "Maya Angelou", "Abraham Lincoln", "Ralph Waldo Emerson", 0);
+        Question question2 = new Question(R.drawable.img_quote_2, "Easy advice to read, difficult advice to follow - who actually?", "Martin Luther King Jr.", "Mother Teresa", "Fred Rogers", "Oprah Winfrey", 1);
+        Question question3 = new Question(R.drawable.img_quote_3, "Insanely inspiring,insanely incorrect(maybe). Who is the true source of this inspiration?", "Nelson Mandela", "Harriet Tubman", "Mahatma Gandh", "Nicholas Klein", 3);
+        Question question4 = new Question(R.drawable.img_quote_4, "A peace worth striving for — who actually reminded us of this?", "Malala Yousafzai", "Martin Luther King Jr.", "Liu Xiaobo", "Dalai Lama", 1);
+        Question question5 = new Question(R.drawable.img_quote_5, "Unfortunately, true — but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Eleanor Roosevelt", "Marilyn Monroe", "Queen Victoria", 0);
+        Question question6 = new Question(R.drawable.img_quote_6, "Here’s the truth, Will Smith did say this, but in which movie?", "Independence Day", "Bad Boys", "Men In Black", "The Pursuit of Happyness", 2);
+
+        Question question7 = new Question(R.drawable.img_quote_7, "Which TV funny gal actually quipped this 1-liner?", "Ellen Degeneres", "Amy Poehler", "Betty White", "Tina Fay", 3);
+        Question question8 = new Question(R.drawable.img_quote_8, "This mayor won’t get my vote — but did he actually give this piece of advice? And if not, who did?", "Forrest Gump, Forrest Gump", "Dorry, Finding Nemo", "Esther Williams", "The Mayor, Jaws", 1);
+        Question question9 = new Question(R.drawable.img_quote_9, "Her heart will go on, but whose heart is it?", "Whitney Houston", "Diana Ross", "Celine Dion ", "Mariah Carey", 0);
+        Question question10 = new Question(R.drawable.img_quote_10, "He’s the king of something alright — to whom does this self-titling line belong to?", "Tony Montana, Scarface", "Joker, The Dark Knight", "Lex Luthor, Batman v Superman", "Jack, Titanic", 3);
+        Question question11 = new Question(R.drawable.img_quote_11, "Is “Grey” synonymous for “wise”? If so, maybe Gandalf did preach this advice. If not, who did?", "Yoda, Star Wars", "Gandalf The Grey, Lord of the Rings", "Dumbledore, Harry Potter", "Uncle Ben, Spider-Man", 0);
+        Question question12 = new Question(R.drawable.img_quote_12, "Houston, we have a problem with this quote — which space-traveler does this catch-phrase actually belong to?", "Han Solo, Star Wars ", "Captain Kirk, Star Trek", "Buzz Lightyear, Toy Story", "Jim Lovell, Apollo 13", 2);
 
         questions.add(question0);
         questions.add(question1);
@@ -229,6 +251,14 @@ public class MainActivity extends AppCompatActivity
         questions.add(question3);
         questions.add(question4);
         questions.add(question5);
+        questions.add(question6);
+
+        questions.add(question7);
+        questions.add(question8);
+        questions.add(question9);
+        questions.add(question10);
+        questions.add(question11);
+        questions.add(question12);
 
         totalCorrect = 0;
         totalQuestions = questions.size();
